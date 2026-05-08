fca6a971eae6d46ce65748dc1b32c2a5d396149a
M.C. <matematikegitiminevu@gmail.com>
2026-05-08 21:48:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # --- LOGOYU YAN MENÜYE EKLEME ---
-st.sidebar.image("mc.png") 
+st.sidebar.image("mc.png", width=150) 
 
 # İsteğe bağlı: Logonun altına ince bir ayırıcı çizgi ve başlık ekleyebilirsiniz
 st.sidebar.markdown("---")
