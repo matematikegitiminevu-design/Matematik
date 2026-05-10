4a0597dffa458f3ae06abc113466eea3a9f38552
M.C. <matematikegitiminevu@gmail.com>
2026-05-10 12:04:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ st.sidebar.image("mc250.png")
 # İsteğe bağlı: Logonun altına ince bir ayırıcı çizgi ve başlık ekleyebilirsiniz
 st.sidebar.markdown("---")
 st.sidebar.write("### CYHN Matematik Portalı")
+st.sidebar.write("### Muharrem CEYHAN")
 st.sidebar.markdown("---")
+
 # --- SAYFA AYARLARI ---
 st.set_page_config(page_title="CYHN | Matematik Portalı", page_icon="mc.png", layout="wide")
 
