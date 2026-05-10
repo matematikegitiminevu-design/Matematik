2d1ed85be665bccf3fefede98ba707502c53c1bf
M.C. <matematikegitiminevu@gmail.com>
2026-05-10 12:17:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.sidebar.image("mc250.png")
 
 # İsteğe bağlı: Logonun altına ince bir ayırıcı çizgi ve başlık ekleyebilirsiniz
 st.sidebar.markdown("---")
-st.sidebar.write("### CYHN Matematik Portalıı")
+st.sidebar.write("### CYHN Matematik Portalı")
 # İmza tarzı, ince yazı tasarımı
 st.sidebar.markdown(
     """
