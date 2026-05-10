2de120adbf3c2f8494927cef018f617f79d4423b
M.C. <matematikegitiminevu@gmail.com>
2026-05-10 12:26:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,7 @@ elif st.session_state["sayfa"] == "sifre_kontrol":
                 **Yasal Uyarı:** 
                 Bu platformda paylaşılan tüm ders PDF notlarının telif hakları doğrudan **Muharrem CEYHAN**'a aittir. 
     Tüm hakları saklıdır.    
-    İçeriklerin tamamının veya bir kısmının, yazarın yazılı izni olmaksızın kopyalanması, çoğaltılması, işlenmesi veya     
-    herhangi bir dijital/basılı mecrada paylaşılması kesinlikle yasaktır. 
+    İçeriklerin tamamının veya bir kısmının, yazarın yazılı izni olmaksızın kopyalanması, çoğaltılması, işlenmesi veya herhangi bir dijital/basılı mecrada paylaşılması **kesinlikle yasaktır**. 
     Sadece kişisel eğitim amaçlıdır..!
             """)
             # Onay kutucuğu
