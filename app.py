d3077b719583ee34dfa4e1e46bffac4098d6266f
M.C. <matematikegitiminevu@gmail.com>
2026-05-14 11:02:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,20 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
         "📐 Soyut Matematik PDF Notları",
         "📝 Boş Alan 3"
     ])
-
+    
+     with tab0:
+        st.subheader("Boş bir kısım")
+        st.warning("Bu bölüm deneme amaçlı açılmıştır.")
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.info("Alan 1")
+        with col2:
+            st.info("Alan 2")
+        with col3:
+            st.info("Alan 3")
+        with col4:
+            st.info("Alan 4")
+            
     with tab1:
         st.subheader("Lineer Cebir Ders Notları")
         st.warning("❗Bu bölümdeki dosyalar güncellenmektedir. PDF ler henüz yüklenmemiştir!")
