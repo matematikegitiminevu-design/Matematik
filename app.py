ebd3cc1397ffcd4abaa96c011660fd9ac9c4ce7a
M.C. <matematikegitiminevu@gmail.com>
2026-05-29 22:23:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
     st.markdown(f"""
     > **Hoş geldin {kullanici}!** Bu arşiv, akademik yolculuğunda sana rehberlik etmek için özenle hazırlanmıştır. 
     > Aşağıdaki sekmeleri kullanarak ders notlarına erişebilir, çalışmalarını derinleştirebilirsin. 
-    > Bir sorun olduğunda yan menüde bulunan iletişim kanallarından bana ulaşabilirsin. **Başarılar.**
+    > *Bir sorun olduğunda yan menüde bulunan iletişim kanallarından bana ulaşabilirsin.* **Başarılar.**
        """)
     # --------------------------
     st.markdown("---")
