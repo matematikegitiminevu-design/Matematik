d060491bd2873e01bbc62f288b736a2eb3ac5dd1
M.C. <matematikegitiminevu@gmail.com>
2026-05-30 22:29:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
     with tab1:
         st.subheader("Algoritma ve Programlama Ders Notu")
         st.warning("❗Algoritma dersinde kullanılmış olan notlar aşağıdadır.")
-        col1, col2, col3, col4, col5 = st.columns(5)
+        col1, col2, col3, col4, col5, col6 = st.columns(6)
         with col1:
             with st.container(border=True):
                 st.markdown("**Algoritmaya Giriş**")
@@ -346,6 +346,10 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
                 st.markdown("**Dosya İşlemleri**")
                 st.link_button("PDF'i Görüntüle", "https://drive.google.com/file/d/1JLnAzmiXzytjKCUzUheIFzhPcj8gODml/view?usp=sharing", use_container_width=True)
         with col5:
+            with st.container(border=True):
+                st.markdown("**Python Notları Toplu**")
+                st.button("⏳ Henüz Yüklenmedi", disabled=True, use_container_width=True, key="ln4")
+        with col6:
             with st.container(border=True):
                 st.markdown("**💻 Özel Notlar (M.C.)**")
                 st.button("⏳ Henüz Yüklenmedi", disabled=True, use_container_width=True, key="ln5")
