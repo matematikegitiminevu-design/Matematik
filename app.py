a41f5fd3214c53b005c74040aeb2bf0c37f73293
M.C. <matematikegitiminevu@gmail.com>
2026-06-06 23:52:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -322,7 +322,7 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
         tr_saat_dilimi = ZoneInfo("Europe/Istanbul")
         # Süre hesaplama motoru
         simdi = datetime.now(tr_saat_dilimi)
-        hedef = datetime.strptime(HEDEF_ZAMAN_GENEL, "%Y-%m-%d %H:%M:%S").replace(tzinfo=tr_saat_dilimi)
+        hedef = datetime.strptime(HEDEF_ZAMAN, "%Y-%m-%d %H:%M:%S").replace(tzinfo=tr_saat_dilimi)
         kalan_sure = hedef - simdi
         
         # Eğer süre henüz dolmadıysa kalan zamanı hesapla ve HTML olarak biçimlendir
