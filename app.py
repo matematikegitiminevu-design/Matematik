1e519b5b12e11cf0d4531ed2fdd4ca8982ce570d
M.C. <matematikegitiminevu@gmail.com>
2026-06-06 23:50:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ elif st.session_state["sayfa"] == "notlar_arsivi":
     ARSIV_BAKIM_MODU = True  
     
     # 📅 BAKIMIN BİTECEĞİ HEDEF TARİH VE SAAT (Yıl-Ay-Gün Saat:Dakika:Saniye)
-    HEDEF_ZAMAN = "2026-06-07 01:00:00"
+    HEDEF_ZAMAN = "2026-06-06 23:52:00"
     # =========================================================================
 
     if ARSIV_BAKIM_MODU:
