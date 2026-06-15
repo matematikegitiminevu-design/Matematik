247e1c20958a9368e1a951beeb3e5149dd396ffb
M.C. <matematikegitiminevu@gmail.com>
2026-06-15 20:33:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -573,7 +573,9 @@ elif st.session_state["sayfa"] == "sifre_kontrol":
         # Başlıklar
         st.markdown('<p class="cyhn-title">CYHN MATEMATİK PORTALI</p>', unsafe_allow_html=True)
         st.markdown('<p class="cyhn-subtitle">Ders Notları Doğrulama Sistemi</p>', unsafe_allow_html=True)
-        st.markdown('<p style="text-align:center; color:#bdc3c7; font-family: monospace; font-size: 13px;">[ Geliştirici: Muharrem Ceyhan ]</p>', unsafe_allow_html=True)
+        st.markdown('<p style="text-align:center; color:#7f8c8d; font-family: \"Georgia\", serif; font-style: italic; font-size: 15px; letter-spacing: 1px;">by Muharrem Ceyhan</p>', unsafe_allow_html=True)
+
+        
         # 📜 Sözleşme
         with st.expander("🔐 Lisans ve Kullanım Sözleşmesi", expanded=False):
             st.markdown(
