a33e7e7c5a010961f690c3cb0237bd3fc3bda46f
M.C. <matematikegitiminevu@gmail.com>
2026-06-18 20:33:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ USERS = {
 # 🛠️ BAKIM MODU AYARLARI
 # =========================================================================
 BAKIM_MODU = False          # Tüm siteyi kapatmak için True yapın
-ARSIV_BAKIM_MODU = True     # Sadece ders arşivini kapatmak için True yapın
+ARSIV_BAKIM_MODU = False      # Sadece ders arşivini kapatmak için True yapın
 
 HEDEF_ZAMAN_GENEL = "2026-06-15 00:00:00"
 HEDEF_ZAMAN_ARSIV = "2026-06-17 00:00:00"
